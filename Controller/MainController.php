708dba40e0961a95da38259de351d1955cfd4b69
Haich <Haich@Haich-Hub96.mshome.net>
2018-05-27 00:11:27 +0200

    Controller V2

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -17,12 +17,14 @@ if ($action==NULL) {
 
 /* Below is a really important section and this is the logic, if you're coding the interface this is where you'll get your data */
 switch ($action){
+    /*Page displaying/ request section */
     case 'login_page':
         include '../View/Login.php';
         break;
     case 'register_page':
         include '../View/Register.php';
         break;
+    /*End Page displaying/ request  section */
     case 'login':
         $username = filter_input(INPUT_POST, 'placeholder_username');
         $password = filter_input(INPUT_POST, 'placeholder_password');
@@ -31,6 +33,13 @@ switch ($action){
             "DBcolum_name"=> $username,
             "DBcolumn_name"=> $password
         );
-        $user_details = DBhelper::sp_SelectWithParams($procedureName, $credentials);
+        $user_details = DBhelper::sp_SelectWithParams($stored_procedure, $credentials);
         break;
+    case 'register_resident':
+        break;
+    case 'register_owner':
+        break;
+    
+        
+        
 }
