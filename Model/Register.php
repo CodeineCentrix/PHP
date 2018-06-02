63124d978c3201a2a506a7970066686b9cd5163c
Haich <Haich@Haich-Hub96.mshome.net>
2018-06-03 01:17:19 +0200

    Added a text file - As a reminder for important things and a register object -  to model a registration process.

---
+++ Model/Register.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Register
+ * Allows a main resident or normal resident to register onto the site.
+ * @author Haich
+ */
+class Register {
+    //put your code here
+}
