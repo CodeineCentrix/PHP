f57351c42d7387b6baa7e82a55dcf3f7e0fcf189
HaichH <healing.legodi@gmail.com>
2018-06-06 16:17:15 +0200

    Merge origin/master
    Conflicts:
    	old vs new changes

--- Resources/View/register_1.php
+++ Resources/View/register_1.php
@@ -4,9 +4,8 @@
 <head>
 <title>Register</title>
 <meta charset="utf-8">
-
 <link rel="stylesheet" href="../Stylesheets/myStyles.css" type="text/css"/>
-		  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body class="MainRes" >
      
