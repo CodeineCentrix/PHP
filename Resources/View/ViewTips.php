e9cacba56203917f5a9f15c4ca3a73ac170b25ae
Mafika Mkhwanazi <Mafika Mkhwanazi@10.122.130.192>
2018-06-08 17:13:10 +0200

    View tips page, still static, not high priority

---
+++ Resources/View/ViewTips.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="../Stylesheets/myStyles.css" type="text/css"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+
+</style>
+</head>
+<body>
+
+<h2>Top Water Saving Tips &amp; Tricks</h2>
+
+
+<div class="container">
+  <img src="../Images/001-student.png" alt="Avatar" style="width:90px">
+  <p><span>Chris Fox.</span>System Administrator.<span name="date" class="date"><i>23June18</i></span></p>
+  <p>John Doe saved us from a web disaster.</p>
+</div>
+
+<div class="container">
+  <img src="../Images/001-student.png" alt="Avatar" style="width:90px">
+  <p><span>Rebecca Flex.</span> Main Resident.<span name="date" class="date"><i>23June18</i></span></p></p>
+  <p >No one is better than John Doe.</p>
+</div>
+<div class="container">
+  <img src="../Images/001-student.png" alt="Avatar" style="width:90px">
+  <p><span>Chris Brown.</span>Resident.<span name="date" class="date"><i>23June18</i></span></p></p>
+  <p>No one is better than John Doe.</p>
+</div>
+<a class="ViewTipsLinks" href="">More tips...</a>
+<p>Remember to share your personal favorite water saving tips <a class="ViewTipsLinks" href="">here.</a></p>
+</body>
+</html>
