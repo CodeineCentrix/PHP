b16de6bda8f56ef6e0bff8d459f8c4691ab3e126
s216316987 <s216316987@M104-26.Mandela.ac.za>
2018-06-11 21:03:17 +0200

    coming soon page

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -213,8 +213,8 @@ switch ($action){
        $fromDate = filter_input(INPUT_POST, 'fromDate');
        $toDate = filter_input(INPUT_POST, 'toDate');
        if (!isset($fromDate)||!isset($toDate)) {
-           $fromDate = date("F j, Y, g:i a", strtotime("-1 months"));
-           $toDate = date();
+           $fromDate = date("F j, Y", strtotime("-1 months"));
+           $toDate = date("F j, Y");
        }
        $readings = $dataAceess->get_readings($house_id, $fromDate, $toDate);
        include '../Resources/View/ViewReadings.php';
